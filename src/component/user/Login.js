16deafa9dc50fc27ab11637738144a105bace97a
maxi <maxi.zhang@arsyun.com>
2020-07-02 09:58:21 +0800

    update

--- src/component/user/Login.js
+++ src/component/user/Login.js
@@ -1,9 +1,22 @@
 import React, {Component} from "react";
+import {Input, Space, Button} from 'antd';
+import {EyeInvisibleOutlined, EyeTwoTone} from '@ant-design/icons';
+import intl from "react-intl-universal";
+
 
 export default class Index extends Component {
     render() {
         return (
-            "this is login"
+            <div className={"login-area"}>
+                <Space direction="vertical">
+                    <Input placeholder={intl.get("USER_NAME")} maxLength={11}/>
+                    <Input.Password
+                        placeholder={intl.get("PASSWORD")}
+                        iconRender={visible => (visible ? <EyeTwoTone/> : <EyeInvisibleOutlined/>)}
+                    />
+                    <Button style={{width:"100%"}} type="primary">{intl.get("CONFIRM")}</Button>
+                </Space>
+            </div>
         )
     }
 }
